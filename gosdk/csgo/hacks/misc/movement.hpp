d511375b499b71d276e40bfc0596f69e99f7cbdf
numero69 <numerouno693@gmail.com>
2020-05-13 19:53:44 +0200

    Update movement.hpp

--- gosdk/csgo/hacks/misc/movement.hpp
+++ gosdk/csgo/hacks/misc/movement.hpp
@@ -14,4 +14,16 @@ namespace CS::Features::Movement {
   }
 
   inline void NoDuckDelay( ) noexcept { Utils::Context::g_pCmd->Buttons.AddFlag( CS::Classes::CUserCmd::IN_BULLRUSH ); }
+  
+    inline void AutoStrafer() noexcept {
+    if ( Utils::Context::g_pLocal->Movetype( ) != CS::Classes::EMovetypes::MOVETYPE_LADDER &&
+         Utils::Context::g_pLocal->Movetype( ) != CS::Classes::EMovetypes::MOVETYPE_NOCLIP &&
+         !Utils::Context::g_pLocal->Flags( ).bCheckFlag( CS::Classes::EEntityFlags::FL_ONGROUND ) ) {
+      if ( Utils::Context::g_pCmd->MouseDX < 0 ) {
+        Utils::Context::g_pCmd->SideMove = -450.0f;
+      } else if ( Utils::Context::g_pCmd->MouseDX > 0 ) {
+        Utils::Context::g_pCmd->SideMove = 450.0f;
+      }
+    }
+  }
 } // namespace CS::Features::Movement
