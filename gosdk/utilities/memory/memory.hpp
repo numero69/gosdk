d9b18ac60fbe02133d6963936fed9056869537cb
gabrielvulk <51248378+cristeigabriel@users.noreply.github.com>
2020-05-14 13:07:42 +0300

    changes

--- gosdk/utilities/memory/memory.hpp
+++ gosdk/utilities/memory/memory.hpp
@@ -9,33 +9,34 @@
 #include "../../dependencies/global.hpp"
 
 namespace Utils::Memory {
-  /* Credits go to the people who made these functions. */
-  inline static void * ScanInterface( const char * module_name, const char * interface_name ) noexcept {
-    const auto module_handle = GetModuleHandleA( module_name );
+  /* Credits go to the people who made these functions and various edits by @cristeigabriel [minimal] */
+  inline static void * ScanInterface( const char * ModuleName, const char * InterfaceName ) noexcept {
+    const auto ModuleHandle = GetModuleHandleA( ModuleName );
 
-    if ( !module_handle )
+    if ( !ModuleHandle )
       return nullptr;
 
-    const auto create_interface_fn =
-        reinterpret_cast<void * (*)( const char *, int * )>( GetProcAddress( module_handle, STR( "CreateInterface" ) ) );
+    const auto CreateInterface_t =
+        reinterpret_cast<void * (*)( const char *, int * )>( GetProcAddress( ModuleHandle, STR( "CreateInterface" ) ) );
 
-    if ( !create_interface_fn )
+    if ( !CreateInterface_t )
       return nullptr;
 
-    const auto interface_address = create_interface_fn( interface_name, nullptr );
+    const auto InterfaceAddress = CreateInterface_t( InterfaceName, nullptr );
 
-    if ( !interface_address )
+    if ( !InterfaceAddress )
       return nullptr;
 
-    return interface_address;
+    return InterfaceAddress;
   }
-  inline static std::uint8_t * FindPattern( const char * module_name, const char * signature ) noexcept {
-    const auto module_handle = GetModuleHandleA( module_name );
 
-    if ( !module_handle )
+  inline static std::uint8_t * FindPattern( const char * ModuleName, const char * Signature ) noexcept {
+    const auto ModuleHandle = GetModuleHandleA( ModuleName );
+
+    if ( !ModuleHandle )
       return nullptr;
 
-    static auto pattern_to_byte = []( const char * pattern ) {
+    static auto PatternToBytes = []( const char * pattern ) {
       auto bytes = std::vector<int>{};
       auto start = const_cast<char *>( pattern );
       auto end = const_cast<char *>( pattern ) + std::strlen( pattern );
@@ -55,59 +56,33 @@ namespace Utils::Memory {
       return bytes;
     };
 
-    auto dos_header = reinterpret_cast<PIMAGE_DOS_HEADER>( module_handle );
-    auto nt_headers =
-        reinterpret_cast<PIMAGE_NT_HEADERS>( reinterpret_cast<std::uint8_t *>( module_handle ) + dos_header->e_lfanew );
+    auto DosHeaders = reinterpret_cast<PIMAGE_DOS_HEADER>( ModuleHandle );
+    auto NtHeaders =
+        reinterpret_cast<PIMAGE_NT_HEADERS>( reinterpret_cast<std::uint8_t *>( ModuleHandle ) + DosHeaders->e_lfanew );
 
-    auto size_of_image = nt_headers->OptionalHeader.SizeOfImage;
-    auto pattern_bytes = pattern_to_byte( signature );
-    auto scan_bytes = reinterpret_cast<std::uint8_t *>( module_handle );
+    auto SizeOfImage = NtHeaders->OptionalHeader.SizeOfImage;
+    auto PatternBytes = PatternToBytes( Signature );
+    auto ScanBytes = reinterpret_cast<std::uint8_t *>( ModuleHandle );
 
-    auto s = pattern_bytes.size( );
-    auto d = pattern_bytes.data( );
+    auto s = PatternBytes.size( );
+    auto d = PatternBytes.data( );
 
-    for ( auto i = 0ul; i < size_of_image - s; ++i ) {
+    for ( auto i = 0ul; i < SizeOfImage - s; ++i ) {
       bool found = true;
 
       for ( auto j = 0ul; j < s; ++j ) {
-        if ( scan_bytes[ i + j ] != d[ j ] && d[ j ] != -1 ) {
+        if ( ScanBytes[ i + j ] != d[ j ] && d[ j ] != -1 ) {
           found = false;
           break;
         }
       }
       if ( found )
-        return &scan_bytes[ i ];
+        return &ScanBytes[ i ];
     }
 
     return nullptr;
   }
-  inline static std::uintptr_t FindPattern( const wchar_t * module, const char * pattern, size_t offset = 0 ) noexcept {
-    static auto id = 0;
-    ++id;
 
-    if ( MODULEINFO module_info;
-         GetModuleInformation( GetCurrentProcess( ), GetModuleHandleW( module ), &module_info, sizeof( module_info ) ) ) {
-      auto start = static_cast<const char *>( module_info.lpBaseOfDll );
-      const auto end = start + module_info.SizeOfImage;
-
-      auto first = start;
-      auto second = pattern;
-
-      while ( first < end && *second ) {
-        if ( *first == *second || *second == '?' ) {
-          ++first;
-          ++second;
-        } else {
-          first = ++start;
-          second = pattern;
-        }
-      }
-
-      if ( !*second )
-        return reinterpret_cast<std::uintptr_t>( const_cast<char *>( start ) + offset );
-    }
-    return 0;
-  }
   template <typename T, typename... Args>
   inline constexpr auto CallVirtualMethod( void * class_base, int index, Args... args ) noexcept {
     return ( ( *reinterpret_cast<T( __thiscall *** )( void *, Args... )>( class_base ) )[ index ] )( class_base, args... );
