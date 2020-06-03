1788f8c3b7d830e4dd4fb315c932ac1ecd77848b
gabrielvulk <51248378+cristeigabriel@users.noreply.github.com>
2020-06-03 23:33:51 +0300

    Not needed

--- gosdk/config/utilities.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "config.hpp"
-
-namespace Config {
-  inline void RunConfig( ) { g_pConfig = std::make_unique<Config::SSettings>( ); }
-
-  inline void ReleaseConfig( ) { g_pConfig.reset( ); }
-} // namespace Config
